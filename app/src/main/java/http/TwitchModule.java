ffcf347d1a57f75c301c52e814c51f1f12f46c82
Nicolas Gonzalez <gonzalezn775@gmail.com>
2019-01-06 22:18:38 -0400

    Provides for use twitch added

--- app/src/main/java/http/TwitchModule.java
+++ app/src/main/java/http/TwitchModule.java
@@ -3,7 +3,9 @@ package http;
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class TwitchModule {
@@ -12,16 +14,22 @@ public class TwitchModule {
 
     @Provides
     public OkHttpClient provideHttpClient() {
-        return null;
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        return new OkHttpClient.Builder().addInterceptor(interceptor).build();
     }
 
     @Provides
     public Retrofit provideRetrofit(String baseURL, OkHttpClient client) {
-        return null;
+        return new Retrofit.Builder()
+            .baseUrl(baseURL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
     }
 
     @Provides
     public TwitchAPI provideTwitchService() {
-        return null;
+        return provideRetrofit(BASE_URL, provideHttpClient()).create(TwitchAPI.class);
     }
 }
